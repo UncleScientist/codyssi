4ae76c4a1e87d32b5d6e36abfd6a773558138594
Uncle Scientist <unclescientist@gmail.com>
2025-03-20 08:39:07 -0400

    Puzzle 1: Compass Calibration

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod puzzle1;
+
+fn main() {
+    println!("Puzzle 1: Compass Calibration");
+    crate::puzzle1::run();
+}
---
+++ src/puzzle1.rs
@@ -0,0 +1,65 @@
+pub fn run() {
+    let data = std::fs::read_to_string("input/puzzle1.txt").unwrap();
+
+    let mut lines = data.split('\n').collect::<Vec<_>>();
+    lines.pop();
+    let ops = lines.pop().unwrap();
+    let nums = lines
+        .iter()
+        .map(|num| num.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    println!(
+        "  part 1 = {}",
+        nums.iter()
+            .skip(1)
+            .zip(ops.chars())
+            .fold(nums[0], |total, (num, op)| {
+                if op == '+' {
+                    total + num
+                } else if op == '-' {
+                    total - num
+                } else {
+                    panic!("invalid op {op}");
+                }
+            })
+    );
+
+    println!(
+        "  part 2 = {}",
+        nums.iter()
+            .skip(1)
+            .zip(ops.chars().rev())
+            .fold(nums[0], |total, (num, op)| {
+                if op == '+' {
+                    total + num
+                } else if op == '-' {
+                    total - num
+                } else {
+                    panic!("invalid op {op}");
+                }
+            })
+    );
+
+    let bignums = nums
+        .chunks(2)
+        .map(|nvec| nvec[0] * 10 + nvec[1])
+        .collect::<Vec<_>>();
+
+    println!(
+        "  part 3 = {}",
+        bignums
+            .iter()
+            .skip(1)
+            .zip(ops.chars().rev())
+            .fold(bignums[0], |total, (num, op)| {
+                if op == '+' {
+                    total + num
+                } else if op == '-' {
+                    total - num
+                } else {
+                    panic!("invalid op {op}");
+                }
+            })
+    );
+}

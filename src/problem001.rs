a85cf58fdff414446f3701d9dabbe28b3e1b64f5
Uncle Scientist <unclescientist@gmail.com>
2025-03-21 08:31:15 -0400

    Problem 1 part 2

--- src/problem001.rs
+++ src/problem001.rs
@@ -9,13 +9,14 @@ pub fn run() -> Result<(), Error> {
         .split('\n')
         .filter(|line| !line.is_empty())
         .collect::<Vec<_>>();
-    println!(
-        "  part 1 = {}",
-        lines
-            .iter()
-            .map(|line| line.parse::<usize>().unwrap())
-            .sum::<usize>()
-    );
+
+    let mut prices = lines
+        .iter()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    prices.sort();
+    println!("  part 1 = {}", prices.iter().sum::<usize>());
+    println!("  part 2 = {}", prices.iter().rev().skip(20).sum::<usize>());
 
     Ok(())
 }

d237823950bc8c36f41c778784e8c21e91066a30
Uncle Scientist <unclescientist@gmail.com>
2025-03-21 08:34:26 -0400

    problem 1 part 3

--- src/problem001.rs
+++ src/problem001.rs
@@ -12,11 +12,18 @@ pub fn run() -> Result<(), Error> {
 
     let mut prices = lines
         .iter()
-        .map(|line| line.parse::<usize>().unwrap())
+        .map(|line| line.parse::<isize>().unwrap())
         .collect::<Vec<_>>();
+    let part3 = prices
+        .iter()
+        .enumerate()
+        .map(|(idx, price)| if idx % 2 == 0 { *price } else { -*price })
+        .sum::<isize>();
+
+    println!("  part 1 = {}", prices.iter().sum::<isize>());
     prices.sort();
-    println!("  part 1 = {}", prices.iter().sum::<usize>());
-    println!("  part 2 = {}", prices.iter().rev().skip(20).sum::<usize>());
+    println!("  part 2 = {}", prices.iter().rev().skip(20).sum::<isize>());
+    println!("  part 3 = {part3}");
 
     Ok(())
 }

20710faea33dac3724b15a819ade253b1de942ab
Uncle Scientist <unclescientist@gmail.com>
2025-03-23 13:23:43 -0400

    Problem 4, part 2

--- src/problem004.rs
+++ src/problem004.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashSet, io::Error};
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    io::Error,
+    str::FromStr,
+};
 
 pub fn run() -> Result<(), Error> {
     let data = std::fs::read_to_string("input/problem004.txt")?;
@@ -9,14 +13,55 @@ pub fn run() -> Result<(), Error> {
         .split('\n')
         .filter(|line| !line.is_empty())
         .collect::<Vec<_>>();
+    let edges = lines
+        .iter()
+        .map(|edge| edge.parse::<Edge>().unwrap())
+        .collect::<Vec<_>>();
 
-    let mut locations = HashSet::new();
-    for line in &lines {
-        let (left, right) = line.split_once(" <-> ").unwrap();
-        locations.insert(left);
-        locations.insert(right);
+    let mut locations = HashMap::<String, HashSet<String>>::new();
+    for edge in &edges {
+        locations
+            .entry(edge.left.clone())
+            .or_default()
+            .insert(edge.right.clone());
+        locations
+            .entry(edge.right.clone())
+            .or_default()
+            .insert(edge.left.clone());
     }
     println!("  part 1 = {}", locations.len());
 
+    let mut queue = VecDeque::new();
+    queue.push_back(("STT".to_string(), 0));
+    let mut visited = HashSet::new();
+    while let Some((loc, time)) = queue.pop_front() {
+        if time > 3 {
+            continue;
+        }
+        if visited.insert(loc.clone()) {
+            for neighbor in &locations[&loc] {
+                queue.push_back((neighbor.clone(), time + 1));
+            }
+        }
+    }
+    println!("  part 2 = {}", visited.len());
     Ok(())
+}
+
+#[derive(Debug)]
+struct Edge {
+    left: String,
+    right: String,
+}
+
+impl FromStr for Edge {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (left, right) = s.split_once(" <-> ").unwrap();
+        Ok(Self {
+            left: left.into(),
+            right: right.into(),
+        })
+    }
 }

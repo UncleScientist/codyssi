92e2946ea5da38ec2103508eac5c3659951a6ca7
Uncle Scientist <unclescientist@gmail.com>
2025-03-23 14:07:09 -0400

    Problem 11, part 3

--- src/problem010.rs
+++ src/problem010.rs
@@ -16,17 +16,35 @@ pub fn run() -> Result<(), Error> {
         lines[0].chars().filter(|ch| ch.is_alphabetic()).count()
     );
 
+    let conversion = |ch| match ch {
+        'a'..='z' => Some((ch as u8 - b'a') as i64 + 1),
+        'A'..='Z' => Some((ch as u8 - b'A') as i64 + 27),
+        _ => None,
+    };
+
     println!(
         "  part 2 = {}",
-        lines[0]
-            .chars()
-            .map(|ch| match ch {
-                'a'..='z' => (ch as u8 - b'a') as usize + 1,
-                'A'..='Z' => (ch as u8 - b'A') as usize + 27,
-                _ => 0,
-            })
-            .sum::<usize>()
+        lines[0].chars().filter_map(conversion).sum::<i64>()
     );
+
+    let mut total = 0;
+    let mut prev = 0;
+    for ch in lines[0].chars() {
+        if let Some(val) = conversion(ch) {
+            total += val;
+            prev = val;
+        } else {
+            prev = (prev * 2) - 5;
+            while prev < 1 {
+                prev += 52;
+            }
+            while prev > 52 {
+                prev -= 52;
+            }
+            total += prev;
+        }
+    }
+    println!("  part 3 = {total}");
 
     Ok(())
 }

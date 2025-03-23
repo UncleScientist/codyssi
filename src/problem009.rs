98608c5858543338e6e4c64a5caecc6e1d2ec031
Uncle Scientist <unclescientist@gmail.com>
2025-03-23 13:55:05 -0400

    Problem 9, part 3

--- src/problem009.rs
+++ src/problem009.rs
@@ -1,7 +1,8 @@
-use std::{io::Error, str::FromStr};
+use std::{collections::HashSet, io::Error, str::FromStr};
 
 pub fn run() -> Result<(), Error> {
     let data = std::fs::read_to_string("input/problem009.txt")?;
+    // let data = std::fs::read_to_string("test.txt")?;
 
     println!("Puzzle 9: Patron Islands");
 
@@ -10,10 +11,10 @@ pub fn run() -> Result<(), Error> {
         .filter(|line| !line.is_empty())
         .collect::<Vec<_>>();
 
-    let points = lines
+    let mut points = lines
         .iter()
         .map(|line| line.parse::<Point>().unwrap())
-        .collect::<Vec<_>>();
+        .collect::<HashSet<_>>();
 
     let dists = points
         .iter()
@@ -32,10 +33,35 @@ pub fn run() -> Result<(), Error> {
         .unwrap();
     println!("  part 2 = {}", closest_to_closest.1);
 
+    let mut total = 0;
+    let mut cur = (Point { x: 0, y: 0 }, 0);
+    while !points.is_empty() {
+        let dist = points
+            .iter()
+            .map(|point| (point, point.dist(&cur.0)))
+            .min_by(|a, b| {
+                if a.1 == b.1 {
+                    if a.0.x == b.0.x {
+                        a.0.y.cmp(&b.0.y)
+                    } else {
+                        a.0.x.cmp(&b.0.x)
+                    }
+                } else {
+                    a.1.cmp(&b.1)
+                }
+            })
+            .unwrap();
+        let dist = (*dist.0, dist.1);
+        points.remove(&dist.0);
+        cur = dist;
+        total += cur.1;
+    }
+    println!("  part 3 = {total}");
+
     Ok(())
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 struct Point {
     x: i64,
     y: i64,

9f0548917d80de4ae20b919dc08ceac679ebffdb
Uncle Scientist <unclescientist@gmail.com>
2025-03-23 13:10:42 -0400

    Problem 3, part 3

--- src/problem003.rs
+++ src/problem003.rs
@@ -20,6 +20,19 @@ pub fn run() -> Result<(), Error> {
         .collect::<Vec<_>>();
 
     println!("  part 1 = {}", pairs.iter().map(|p| p.1).sum::<u32>());
-    println!("  part 2 = {}", pairs.iter().map(|p| p.0).sum::<usize>());
+    let mut num = pairs.iter().map(|p| p.0).sum::<usize>();
+    println!("  part 2 = {num}");
+
+    let num_to_char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#"
+        .chars()
+        .collect::<Vec<_>>();
+    let mut result = Vec::new();
+    while num > 0 {
+        let unit = num % 65;
+        result.push(num_to_char[unit]);
+        num = num / 65;
+    }
+
+    println!("  part 3 = {}", result.iter().rev().collect::<String>());
     Ok(())
 }

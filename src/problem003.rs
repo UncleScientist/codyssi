33a9524d31035ac550b5c98ed4cba694fa72b5a2
Uncle Scientist <unclescientist@gmail.com>
2025-03-23 13:00:03 -0400

    Problem 3, part 2

--- src/problem003.rs
+++ src/problem003.rs
@@ -9,16 +9,17 @@ pub fn run() -> Result<(), Error> {
         .split('\n')
         .filter(|line| !line.is_empty())
         .collect::<Vec<_>>();
+    let pairs = lines
+        .iter()
+        .map(|line| {
+            let (strval, base) = line.split_once(' ').unwrap();
+            let base = base.parse::<u32>().unwrap();
+            let num = usize::from_str_radix(strval, base).unwrap();
+            (num, base)
+        })
+        .collect::<Vec<_>>();
 
-    println!(
-        "  part 1 = {}",
-        lines
-            .iter()
-            .map(|line| {
-                let (_, base) = line.split_once(' ').unwrap();
-                base.parse::<usize>().unwrap()
-            })
-            .sum::<usize>()
-    );
+    println!("  part 1 = {}", pairs.iter().map(|p| p.1).sum::<u32>());
+    println!("  part 2 = {}", pairs.iter().map(|p| p.0).sum::<usize>());
     Ok(())
 }

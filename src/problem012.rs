6856814ea0f179dfbe5c60e774d8db679d59038c
Uncle Scientist <unclescientist@gmail.com>
2025-03-24 07:40:25 -0400

    Problem 12, part 3

--- src/problem012.rs
+++ src/problem012.rs
@@ -17,16 +17,28 @@ pub fn run() -> Result<(), Error> {
             .sum::<usize>()
     );
 
-    let part2 = lines.iter().map(reduce).collect::<Vec<_>>();
+    let part2 = lines
+        .iter()
+        .map(|line| reduce_by(line, reduce_with_hyphens))
+        .collect::<Vec<_>>();
     println!(
         "  part 2 = {}",
         part2.iter().map(|line| line.len()).sum::<usize>()
     );
 
+    let part3 = lines
+        .iter()
+        .map(|line| reduce_by(line, reduce_without_hyphens))
+        .collect::<Vec<_>>();
+    println!(
+        "  part 3 = {}",
+        part3.iter().map(|line| line.len()).sum::<usize>()
+    );
+
     Ok(())
 }
 
-fn reduce<S: AsRef<str>>(s: S) -> String {
+fn reduce_by<S: AsRef<str>>(s: S, test: fn(char, char) -> bool) -> String {
     let mut chars = s.as_ref().chars().collect::<Vec<_>>();
     let mut result = "".to_string();
     let mut removed = true;
@@ -36,9 +48,7 @@ fn reduce<S: AsRef<str>>(s: S) -> String {
         result = "".to_string();
         for i in 0..chars.len() {
             if i + 1 < chars.len() {
-                if (chars[i].is_numeric() && (chars[i + 1].is_alphabetic() || chars[i + 1] == '-'))
-                    || ((chars[i].is_alphabetic() || chars[i] == '-') && chars[i + 1].is_numeric())
-                {
+                if test(chars[i], chars[i + 1]) {
                     result.push_str(&chars[i + 2..].iter().collect::<String>());
                     removed = true;
                     break;
@@ -54,34 +64,49 @@ fn reduce<S: AsRef<str>>(s: S) -> String {
     result
 }
 
+fn reduce_with_hyphens(ch1: char, ch2: char) -> bool {
+    (ch1.is_numeric() && (ch2.is_alphabetic() || ch2 == '-'))
+        || ((ch1.is_alphabetic() || ch1 == '-') && ch2.is_numeric())
+}
+
+fn reduce_without_hyphens(ch1: char, ch2: char) -> bool {
+    (ch1.is_numeric() && ch2.is_alphabetic()) || (ch1.is_alphabetic() && ch2.is_numeric())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn test_reduce_baa3() {
-        assert_eq!("ba".to_string(), reduce("baa3"));
+        assert_eq!("ba".to_string(), reduce_by("baa3", reduce_with_hyphens));
     }
 
     #[test]
     fn test_reduce_321ab() {
-        assert_eq!("3".to_string(), reduce("321ab"));
+        assert_eq!("3".to_string(), reduce_by("321ab", reduce_with_hyphens));
     }
 
     #[test]
     fn test_reduce_a7b() {
-        assert_eq!("b".to_string(), reduce("a7b"));
+        assert_eq!("b".to_string(), reduce_by("a7b", reduce_with_hyphens));
     }
 
     #[test]
     fn test_reduce_zdash4() {
-        assert_eq!("z".to_string(), reduce("z-4"));
+        assert_eq!("z".to_string(), reduce_by("z-4", reduce_with_hyphens));
     }
 
     #[test]
     fn test_sample_input() {
-        let t1 = reduce("tv8cmj0i2951190z5w44fe205k542l5818ds05ib425h9lj260ud38-l6a06");
-        let t2 = reduce("a586m0eeuqqvt5-k-8434hb27ytha3i75-lw23-0cj856l7zn8234a05eron");
+        let t1 = reduce_by(
+            "tv8cmj0i2951190z5w44fe205k542l5818ds05ib425h9lj260ud38-l6a06",
+            reduce_with_hyphens,
+        );
+        let t2 = reduce_by(
+            "a586m0eeuqqvt5-k-8434hb27ytha3i75-lw23-0cj856l7zn8234a05eron",
+            reduce_with_hyphens,
+        );
         assert_eq!(18, t1.len() + t2.len());
     }
 }

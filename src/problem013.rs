9e677360e5997ccdc9099446c7cd8a4fe750ac20
Uncle Scientist <unclescientist@gmail.com>
2025-03-25 07:44:35 -0400

    Problem 13, part 3

--- src/problem013.rs
+++ src/problem013.rs
@@ -2,6 +2,7 @@ use std::{collections::HashMap, io::Error};
 
 pub fn run() -> Result<(), Error> {
     let data = std::fs::read_to_string("input/problem013.txt")?;
+    // let data = std::fs::read_to_string("test.txt")?;
 
     println!("Puzzle 13: Windy Bargain");
 
@@ -15,7 +16,7 @@ pub fn run() -> Result<(), Error> {
         let words = line.split(' ').collect::<Vec<_>>();
         starting_balances.insert(words[0].into(), words[2].parse::<i64>().unwrap());
     }
-    println!("{starting_balances:?}");
+    // println!("{starting_balances:?}");
 
     let mut part1 = starting_balances.clone();
     for line in sections[1].split('\n') {
@@ -24,8 +25,8 @@ pub fn run() -> Result<(), Error> {
         }
         let words = line.split(' ').collect::<Vec<_>>();
         let amount = words[5].parse::<i64>().unwrap();
-        *part1.get_mut(words[1].into()).unwrap() -= amount;
-        *part1.get_mut(words[3].into()).unwrap() += amount;
+        *part1.get_mut(words[1]).unwrap() -= amount;
+        *part1.get_mut(words[3]).unwrap() += amount;
     }
     let mut balances = part1.values().copied().collect::<Vec<_>>();
     balances.sort();
@@ -46,6 +47,75 @@ pub fn run() -> Result<(), Error> {
     let mut balances = part2.values().copied().collect::<Vec<_>>();
     balances.sort();
     println!("  part 2 = {}", balances.iter().rev().take(3).sum::<i64>());
+
+    let mut part3 = starting_balances.clone();
+    let mut owes = HashMap::<String, Vec<(String, i64)>>::new();
+    for line in sections[1].split('\n') {
+        if line.is_empty() {
+            continue;
+        }
+        let words = line.split(' ').collect::<Vec<_>>();
+        let from = words[1].to_string();
+        let to = words[3].to_string();
+        let amount = words[5].parse::<i64>().unwrap();
+
+        if amount > part3[&from] {
+            // split into a paid + debt
+            let can_pay = part3[&from];
+            let debt = amount - can_pay;
+            *part3.get_mut(&from).unwrap() = 0;
+            *part3.get_mut(&to).unwrap() += can_pay;
+            owes.entry(from.clone())
+                .or_default()
+                .push((to.clone(), debt));
+        } else {
+            // can pay full amount
+            *part3.get_mut(&from).unwrap() -= amount;
+            *part3.get_mut(&to).unwrap() += amount;
+        }
+
+        // println!("{line}:");
+        // println!("  {part3:?}");
+        // println!("  {owes:?}");
+
+        // check the `owes` map and see if we can settle debts
+        loop {
+            let mut money_moved = false;
+            for person in owes.iter_mut() {
+                let mut amount = part3[person.0];
+                // println!("  {} has {amount}", person.0);
+                if amount == 0 {
+                    continue;
+                }
+
+                for debt in person.1 {
+                    let payment = debt.1.min(amount);
+                    if payment == 0 {
+                        continue;
+                    }
+                    debt.1 -= payment;
+                    amount -= payment;
+                    *part3.get_mut(&debt.0).unwrap() += payment;
+                    *part3.get_mut(person.0).unwrap() -= payment;
+                    money_moved = true;
+                    // println!("  -> paying {payment} to {}", debt.0);
+                    if amount == 0 {
+                        break;
+                    }
+                }
+            }
+            if !money_moved {
+                break;
+            }
+        }
+        // println!("After Payments:");
+        // println!("  {part3:?}");
+        // println!("  {owes:?}");
+        // println!("--");
+    }
+    let mut balances = part3.values().copied().collect::<Vec<_>>();
+    balances.sort();
+    println!("  part 3 = {}", balances.iter().rev().take(3).sum::<i64>());
 
     Ok(())
 }

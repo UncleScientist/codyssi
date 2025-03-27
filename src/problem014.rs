820714a741ddb9d3a52ff9896eb6662dacf1d96f
Uncle Scientist <unclescientist@gmail.com>
2025-03-27 07:08:43 -0400

    Problem 14, part 2

--- src/problem014.rs
+++ src/problem014.rs
@@ -1,4 +1,7 @@
-use std::io::Error;
+use std::{
+    collections::{BTreeSet, HashMap, HashSet},
+    io::Error,
+};
 
 pub fn run() -> Result<(), Error> {
     let data = std::fs::read_to_string("input/problem014.txt")?;
@@ -17,12 +20,42 @@ pub fn run() -> Result<(), Error> {
         grid.push(line.split(' ').map(|num| num.parse().unwrap()).collect());
     }
 
+    let gridsize = grid.len();
+
     let mut safest = i64::MAX;
-    for i in 0..50 {
+    for i in 0..gridsize {
         let rowsum = grid[i].iter().sum::<i64>();
-        let colsum = (0..50).map(|col| grid[i][col]).sum::<i64>();
+        let colsum = (0..gridsize).map(|col| grid[i][col]).sum::<i64>();
         safest = safest.min(rowsum.min(colsum));
     }
     println!("  part 1 = {safest}");
+
+    let mut queue = BTreeSet::from([(grid[0][0], (0usize, 0usize))]);
+    let mut visited = HashSet::new();
+    let mut costs = HashMap::<(usize, usize), i64>::new();
+    while let Some((cost, pos)) = queue.pop_first() {
+        if pos == (14, 14) {
+            println!("  part 2 = {cost}");
+            break;
+        }
+        if visited.insert(pos) {
+            for dir in [(1, 0), (0, 1)] {
+                let newpos = (pos.0 + dir.0, pos.1 + dir.1);
+                if newpos.0 >= grid.len() || newpos.1 >= grid[0].len() {
+                    continue;
+                }
+
+                let newcost = cost + grid[newpos.0][newpos.1];
+                if !visited.contains(&newpos) {
+                    let entry = costs.entry(pos).or_insert(i64::MAX);
+                    if newcost < *entry {
+                        *entry = newcost;
+                    }
+                }
+                queue.insert((newcost, newpos));
+            }
+        }
+    }
+
     Ok(())
 }

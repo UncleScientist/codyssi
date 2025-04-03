3fa57f4505d180a57763df5cf12416f6412bfba8
Uncle Scientist <unclescientist@gmail.com>
2025-04-03 07:43:03 -0400

    Problem 19, part 3

--- src/problem019.rs
+++ src/problem019.rs
@@ -1,17 +1,16 @@
 use std::{io::Error, str::FromStr};
 
 pub fn run() -> Result<(), Error> {
-    let lines = crate::read_sections(19, "Artifacts at Atlantis")?;
-    let artifacts = lines[0]
+    let sections = crate::read_sections(19, "Artifacts at Atlantis")?;
+    let artifacts = sections[0]
         .iter()
         .map(|line| line.parse::<Artifact>().unwrap())
         .collect::<Vec<_>>();
 
     let mut root: Option<Box<BinaryTree>> = None;
 
-    let mut _codelist = Vec::new();
     for artifact in artifacts {
-        insert(&mut root, artifact, 1, &mut _codelist);
+        insert(&mut root, artifact, 1);
     }
 
     let mut layer_sum = Vec::new();
@@ -25,11 +24,30 @@ pub fn run() -> Result<(), Error> {
         "  part 1 = {}",
         layer_sum.iter().max().unwrap() * (layer_sum.len() - 1)
     );
+    // 1146586780
 
     let missing_one = "asdfgh | 500000".parse::<Artifact>().unwrap();
     let mut codelist = Vec::new();
-    insert(&mut root, missing_one, 1, &mut codelist);
+    find(&root, &missing_one, &mut codelist);
     println!("  part 2 = {}", codelist.join("-"));
+    // invlzfI-gecqIRf-NpYaQSb-batqlnG-ZzpMEmN-XUasQMx-KZRhJMk-RBWFcpT-yLgLIBv-TXZsSvS-SEviNEC
+
+    let artifact1 = sections[1][0].parse::<Artifact>().unwrap();
+    let artifact2 = sections[1][1].parse::<Artifact>().unwrap();
+    let mut path1 = Vec::new();
+    let mut path2 = Vec::new();
+
+    find(&root, &artifact1, &mut path1);
+    find(&root, &artifact2, &mut path2);
+
+    let first_mismatch = path1
+        .iter()
+        .zip(path2.iter())
+        .enumerate()
+        .find(|(_, (p1, p2))| p1 != p2)
+        .unwrap();
+
+    println!("  part 3 = {}", path1[first_mismatch.0 - 1]);
 
     Ok(())
 }
@@ -59,18 +77,23 @@ fn traverse<F: FnMut(&Artifact)>(tree: &Option<Box<BinaryTree>>, callback: &mut 
     }
 }
 
-fn insert(
-    tree: &mut Option<Box<BinaryTree>>,
-    mut node: Artifact,
-    layer: usize,
-    codelist: &mut Vec<String>,
-) {
+fn find(tree: &Option<Box<BinaryTree>>, node: &Artifact, codelist: &mut Vec<String>) {
     if let Some(entry) = tree {
         codelist.push(entry.node.code.clone());
         if node.id < entry.node.id {
-            insert(&mut entry.left, node, layer + 1, codelist);
+            find(&entry.left, node, codelist);
         } else {
-            insert(&mut entry.right, node, layer + 1, codelist);
+            find(&entry.right, node, codelist);
+        }
+    }
+}
+
+fn insert(tree: &mut Option<Box<BinaryTree>>, mut node: Artifact, layer: usize) {
+    if let Some(entry) = tree {
+        if node.id < entry.node.id {
+            insert(&mut entry.left, node, layer + 1);
+        } else {
+            insert(&mut entry.right, node, layer + 1);
         }
     } else {
         node.layer = layer;

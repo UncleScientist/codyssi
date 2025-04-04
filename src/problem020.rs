6d92f386597a3609902ba813feab31e0a6d1b8a8
Uncle Scientist <unclescientist@gmail.com>
2025-04-04 08:38:03 -0400

    Incorrect solution for Puzzle 20, part 2

--- src/problem020.rs
+++ src/problem020.rs
@@ -15,17 +15,26 @@ pub fn run() -> Result<(), Error> {
 
     let mut cube = Cube::new(80);
 
+    // cube._print();
+    // println!("{:?}", instructions[0]);
     cube.action(instructions[0]);
+    // cube._print();
     for (twist, inst) in twists.iter().zip(instructions.iter().skip(1)) {
+        // println!("{twist:?}");
         cube.twist(*twist);
+        // println!("{inst:?}");
         cube.action(*inst);
+        // cube._print();
     }
     let mut values = cube.absorption;
     values.sort();
     println!(
         "  part 1 = {}",
-        values.iter().rev().take(2).product::<usize>()
+        values.iter().rev().take(2).product::<u128>()
     );
+
+    println!("  part 2 = {}", cube.dominant_sums());
+    // incorrect: 60449500975162539098880
 
     Ok(())
 }
@@ -38,9 +47,89 @@ struct CubeFace {
 impl CubeFace {
     fn new(size: usize) -> Self {
         Self {
-            face: vec![vec![0; size]; size],
+            face: vec![vec![1; size]; size],
         }
     }
+
+    fn _print(&self) {
+        for row in &self.face {
+            for cell in row {
+                print!(" {cell:2}");
+            }
+            println!();
+        }
+    }
+
+    // 1 2 3                    7 4 1
+    // 4 5 6 -> rotate right -> 8 5 2
+    // 7 8 9                    9 6 3
+    fn rotate_right(&mut self) {
+        let size = self.face.len();
+        let mut result = vec![vec![0; size]; size];
+        for i in 0..size {
+            for j in 0..size {
+                result[j][size - 1 - i] = self.face[i][j];
+            }
+        }
+        self.face = result;
+    }
+
+    // 1 2 3                   3 6 9
+    // 4 5 6 -> rotate left -> 2 5 8
+    // 7 8 9                   1 4 7
+    fn rotate_left(&mut self) {
+        let size = self.face.len();
+        let mut result = vec![vec![0; size]; size];
+        for i in 0..size {
+            for j in 0..size {
+                result[size - 1 - j][i] = self.face[i][j];
+            }
+        }
+        self.face = result;
+    }
+
+    fn add_all(&mut self, amt: u8) {
+        for row in &mut self.face {
+            for cell in row {
+                *cell = add_cell(*cell, amt);
+            }
+        }
+    }
+
+    fn add_row(&mut self, row: usize, amt: u8) {
+        for cell in &mut self.face[row] {
+            *cell = add_cell(*cell, amt);
+        }
+    }
+
+    fn add_col(&mut self, col: usize, amt: u8) {
+        for row in &mut self.face {
+            row[col] = add_cell(row[col], amt);
+        }
+    }
+
+    fn dominant_sum(&self) -> u128 {
+        let rowmax = self
+            .face
+            .iter()
+            .map(|row| row.iter().fold(0u128, |sum, entry| sum + (*entry as u128)))
+            .max()
+            .unwrap();
+        let mut colmax = 0;
+        for col in 0..self.face.len() {
+            colmax = colmax.max(self.face.iter().map(|row| row[col] as u128).sum::<u128>())
+        }
+        rowmax.max(colmax)
+    }
+}
+
+fn add_cell(val: u8, amt: u8) -> u8 {
+    let mut newval = val + amt;
+    while newval > 100 {
+        newval -= 100;
+    }
+    newval
+    // (val + amt - 1) % 100 + 1
 }
 
 #[derive(Debug)]
@@ -52,7 +141,7 @@ struct Cube {
     right: usize,
     back: usize,
     forward: usize, // this is the one facing the user
-    absorption: [usize; 6],
+    absorption: [u128; 6],
 }
 
 impl Cube {
@@ -69,15 +158,35 @@ impl Cube {
         }
     }
 
+    fn _print(&self) {
+        println!("forward:");
+        self.faces[self.forward]._print();
+        println!("back:");
+        self.faces[self.back]._print();
+        println!("left:");
+        self.faces[self.left]._print();
+        println!("right:");
+        self.faces[self.right]._print();
+        println!("top:");
+        self.faces[self.top]._print();
+        println!("bottom:");
+        self.faces[self.bottom]._print();
+    }
+
     fn action(&mut self, inst: Instruction) {
-        let size = self.faces[0].face.len();
+        let size = self.faces[0].face.len() as u128;
         match inst {
-            Instruction::Face(amt) => self.absorption[self.forward] += (amt as usize) * size * size,
-            Instruction::Row(_, amt) => {
-                self.absorption[self.forward] += (amt as usize) * size;
+            Instruction::Face(amt) => {
+                self.faces[self.forward].add_all(amt);
+                self.absorption[self.forward] += (amt as u128) * size * size;
             }
-            Instruction::Col(_, amt) => {
-                self.absorption[self.forward] += (amt as usize) * size;
+            Instruction::Row(row, amt) => {
+                self.faces[self.forward].add_row(row, amt);
+                self.absorption[self.forward] += (amt as u128) * size;
+            }
+            Instruction::Col(col, amt) => {
+                self.faces[self.forward].add_col(col, amt);
+                self.absorption[self.forward] += (amt as u128) * size;
             }
         }
     }
@@ -90,6 +199,8 @@ impl Cube {
                 self.back = self.right;
                 self.right = self.forward;
                 self.forward = tmp;
+                self.faces[self.top].rotate_left();
+                self.faces[self.bottom].rotate_right();
             }
             Twist::Right => {
                 let tmp = self.right;
@@ -97,6 +208,8 @@ impl Cube {
                 self.back = self.left;
                 self.left = self.forward;
                 self.forward = tmp;
+                self.faces[self.top].rotate_right();
+                self.faces[self.bottom].rotate_left();
             }
             Twist::Up => {
                 let tmp = self.top;
@@ -104,6 +217,8 @@ impl Cube {
                 self.back = self.bottom;
                 self.bottom = self.forward;
                 self.forward = tmp;
+                self.faces[self.left].rotate_right();
+                self.faces[self.right].rotate_left();
             }
             Twist::Down => {
                 let tmp = self.bottom;
@@ -111,8 +226,14 @@ impl Cube {
                 self.back = self.top;
                 self.top = self.forward;
                 self.forward = tmp;
+                self.faces[self.left].rotate_left();
+                self.faces[self.right].rotate_right();
             }
         }
+    }
+
+    fn dominant_sums(&self) -> u128 {
+        self.faces.iter().map(|face| face.dominant_sum()).product()
     }
 }
 
@@ -160,6 +281,43 @@ impl From<char> for Twist {
             'D' => Self::Down,
             'U' => Self::Up,
             _ => panic!("Invalid twist: {value}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_rotate_right() {
+        let v1 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let v2 = vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]];
+        let mut face = CubeFace::new(3);
+        face.face = v1;
+        face.rotate_right();
+        assert_eq!(v2, face.face);
+    }
+
+    #[test]
+    fn test_rotate_left() {
+        let v1 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let v2 = vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7]];
+        let mut face = CubeFace::new(3);
+        face.face = v1;
+        face.rotate_left();
+        assert_eq!(v2, face.face);
+    }
+
+    #[test]
+    fn test_mod_stuff() {
+        assert_eq!(39, add_cell(1, 38));
+        assert_eq!(72, add_cell(1, 71));
+        assert_eq!(7, add_cell(68, 39));
+        assert_eq!(100, add_cell(99, 1));
+        assert_eq!(98, add_cell(99, 99));
+        for i in 1..=100 {
+            assert_eq!(i, add_cell(i, 100));
         }
     }
 }
